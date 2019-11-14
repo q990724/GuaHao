2f2d3741aec0566c9f713db19ae26cb9ae9164c6
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:35 +0800

    Create preTitle.vue

---
+++ src/components/index/mhp/preTitle.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="con_title">
+      <span class="goback" @click="goback"> &lt;</span>
+      <span class="pre_title">预约挂号</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    goback(){
+      this.$router.push("/parent");
+    }
+  },
+}
+</script>
+<style scoped>
+.container{
+  height: 45px;
+  position: fixed;
+  top:0px;
+  left:0px;
+  right:0px;
+  background: #fff;
+  /* overflow: hidden; */
+}
+.con_title{
+  height:45px;
+  background: #fff;
+  line-height: 45px;
+  font-size: 18px;
+  font-weight: 400;
+  border-bottom:1px solid #aaa;
+}
+.goback{
+  position: relative;
+  left:-120px;
+  font-size: 30px;
+  line-height: 45px;
+}
+</style>
