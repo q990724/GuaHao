c95f07c995142dfc09995b0b27e73ee87d6cb64c
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:41 +0800

    Create wysearch.vue

---
+++ src/components/index/mhp/wysearch.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="search">
+    <a href="">
+      <div class="content">
+        <img src="../../../../public/images/index/22toGuahao/gray-search.cfda5be2b31e1669e46a9bc82bf199f2.png" alt="">
+        <i>搜索医院，医生，科室，疾病，药品</i>
+      </div>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+.search{
+  width:385px;
+  height:45px;
+  /* position: relative; */
+  margin-top: 55px;
+  margin-bottom: 10px;
+  border-bottom:1px solid #aaa;
+  /* top:45px; */
+}
+.search>a>.content{
+  width:310px;
+  height:32px;
+  margin:10px 13px;
+  background-color: #ebecf1;
+  border-radius: 15px;
+  color:#999;
+  font-size:10px;
+  line-height: 32px;
+}
+.search>a>.content>img{
+  width:18px;
+  height:18px;
+  position: relative;
+  top:7px;
+  left:-40px;
+}
+.search>a>.content>i{
+  position: relative;
+  left:-33px;
+}
+</style>
