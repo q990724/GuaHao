28a49818e1b1055a29079c33f0cc10c66a54c6de
mhp <359372260@qq.com>
2019-11-14 23:42:03 +0800

    Update preTitle.vue

--- src/components/index/mhp/preTitle.vue
+++ src/components/index/mhp/preTitle.vue
@@ -22,26 +22,26 @@ export default {
 </script>
 <style scoped>
 .container{
-  height: 45px;
+  height: 2.8125rem;
   position: fixed;
-  top:0px;
-  left:0px;
-  right:0px;
+  top:0rem;
+  left:0rem;
+  right:0rem;
   background: #fff;
   /* overflow: hidden; */
 }
 .con_title{
-  height:45px;
+  height:2.8125rem;
   background: #fff;
-  line-height: 45px;
-  font-size: 18px;
+  line-height: 2.8125rem;
+  font-size: 1.125rem;
   font-weight: 400;
-  border-bottom:1px solid #aaa;
+  border-bottom:.0625rem solid #aaa;
 }
 .goback{
   position: relative;
-  left:-120px;
-  font-size: 30px;
-  line-height: 45px;
+  left:-7.5rem;
+  font-size: 1.875rem;
+  line-height: 2.8125rem;
 }
 </style>
