8011c26764a2a35692f8b42fea9577ad98d35e7b
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/vuex.js'
-
-//1:完整引入mint-ui组件库
+// import store from './store'
+// 引入和配置mint-ui组件库
+// 1.完整引入mint-ui组件库
 import MintUI from "mint-ui"
-//2.单独引入mint-ui样式文件
+// 2.单独引入mint-ui样式文件
 import "mint-ui/lib/style.css"
-//3:将mintui对象注册在vue实例中
+// 
+
+import {TreeSelect} from "vant";
+import "vant/lib/tree-select/style"
+
+Vue.use(TreeSelect);
+
+// Vue.use(vant);
+// 3.将mint-ui对象注册到vue实例中
 Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
-
-
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount('#app')
