77f3911e6f85e2e412d68373659f2995c4bd161b
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:26 +0800

    Create parent.vue

---
+++ src/components/index/mhp/parent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <wytitle class="title"></wytitle>
+    <wysearch></wysearch>
+    <div class="entry_major">
+      <quik></quik>
+      <toGuahao></toGuahao>
+    </div>
+    <normal></normal>
+    <div class="bottom"></div>
+  </div>
+</template>
+<script>
+import wytitle from "./wytitle";
+import wysearch from "./wysearch";
+import quik from "./quik";
+import toGuahao from "./toGuahao";
+import normal from "./normal";
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    "wytitle":wytitle,
+    "wysearch":wysearch,
+    "quik":quik,
+    "toGuahao":toGuahao,
+    "normal":normal
+  },
+  methods: {
+
+  },
+}
+</script>
+<style scoped>
+.entry_major{ 
+  display: flex;
+}
+.bottom{
+  /* overflow-y:scroll; */
+}
+</style>
