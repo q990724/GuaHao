e6217cb22fe4a1a65470ae17a317e0dbf8ed3d45
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:56 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import parent from "./components/index/mhp/parent.vue"
+import city from "./components/index/mhp/city.vue"
+import preTitle from "./components/index/mhp/preTitle.vue"
+import wysearch from "./components/index/mhp/wysearch.vue";
+import preGuahao from "./components/index/mhp/preGuahao.vue";
 
-Vue.use(VueRouter)
-
-const routes = [
-
-]
-
-const router = new VueRouter({
-  routes
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {path:"/preGuahao",component:preGuahao},
+    {path:"/wysearch",component:wysearch},
+    {path:"/preTitle",component:preTitle},
+    {path:"/city",component:city},
+    {path:"/parent",component:parent},
+  ]
 })
-
-export default router
