b184f13ce3aba09e8704618b977e0d70ecf760db
w1322174203 <57716097+w1322174203@users.noreply.github.com>
2019-11-15 00:32:56 +0800

    me页面尾部完成

---
+++ src/components/me/wf/community.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class = "community-item">
+    <router-link to = "" class = "community-item-img">
+      <img src="../../../../public/images/me/me-Community/icon-sns-watch.png" alt="">
+      <span>我的关注</span>
+    </router-link>
+    <router-link to = "" class = "community-item-img">
+      <img src="../../../../public/images/me/me-Community/icon-sns-question.png" alt="">
+      <span>我的提问</span>
+    </router-link>
+    <router-link to = "" class = "community-item-img">
+      <img src="../../../../public/images/me/me-Community/icon-sns-post.png" alt="">
+      <span>我的发帖</span>
+    </router-link>
+    <router-link to = "" class = "community-item-img">
+      <img src="../../../../public/images/me/me-Community/icon-sns-more.png" alt="">
+      <span>更多</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .community-item{
+    display: flex;
+    justify-content: space-around; 
+    border-top: .0625rem solid  #eee;
+  }
+  .community-item-img{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    color: #83889a;
+    padding: 1.035rem 0 .8125rem;
+    font-size: .8125rem;
+    width: 17%;
+  }
+  .community-item-img img{
+    width: 1.5rem;
+    height: 1.5rem;
+    display: block;
+    margin-bottom:.5rem; 
+  }
+</style>
---
+++ src/components/me/wf/tools.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class = "whole">
+    <div class = "tools">
+      <h2>常用工具</h2>
+      <tools-item></tools-item>
+    </div>
+    <div class = "community">
+      <h2>我的社区</h2>
+      <community></community>
+    </div>
+    <div class = "healthy">
+      <h2>健康档案</h2>
+      <healthy></healthy>
+    </div>
+  </div>
+</template>
+
+<script>
+  import toolsItem from "./tools-item.vue"
+  import community from "./community.vue"
+  import healthy from "./healthy.vue"
+  export default {
+    components:{
+      "toolsItem" : toolsItem,
+      "community" : community,
+      "healthy" : healthy
+    }
+  }
+</script>
+
+<style scoped>
+  .whole{
+    background: #f8f8f8;
+  }
+  .tools,.community{
+    background: #fff;
+    border-bottom: .625rem;
+    margin-bottom: .6875rem;
+  }
+  .tools h2,.community h2,.healthy h2{
+    font-size: 1rem;
+    line-height: 2.75rem;
+    color: #83889a;
+    font-weight: 500;
+  }
+  .healthy{
+    height: 15.18rem;
+    background: #fff;
+  }
+</style>
+
