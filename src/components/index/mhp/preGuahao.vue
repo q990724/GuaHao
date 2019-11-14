a0b2aafc1354d30d29e205c0c43ba348f0460cb2
mhp <359372260@qq.com>
2019-11-14 23:41:59 +0800

    Update preGuahao.vue

--- src/components/index/mhp/preGuahao.vue
+++ src/components/index/mhp/preGuahao.vue
@@ -6,12 +6,29 @@
       <ul>
         <li class="sanjia">
           <div>
-            <img src="" alt="">
+            <img src="../../../../public/images/index/22toGuahao/01.预约挂号-医院列表+copy+8@2x.png" alt="">
+          </div>
+          <span>三甲体检</span>
+        </li>
+        <li class="sanjia">
+          <div>
+            <img src="../../../../public/images/index/22toGuahao/01.预约挂号-医院列表+copy+8@2x.png" alt="">
+          </div>
+          <span>三甲体检</span>
+        </li>
+        <li class="sanjia">
+          <div>
+            <img src="../../../../public/images/index/22toGuahao/01.预约挂号-医院列表+copy+8@2x.png" alt="">
+          </div>
+          <span>三甲体检</span>
+        </li>
+        <li class="sanjia">
+          <div>
+            <img src="../../../../public/images/index/22toGuahao/01.预约挂号-医院列表+copy+8@2x.png" alt="">
           </div>
           <span>三甲体检</span>
         </li>
       </ul>
-
     </div>
   </div>
 </template>
@@ -32,16 +49,29 @@ export default {
 </script>
 <style scoped>
 templete{
-    height:1200px;
-}
-wysearch{
-  border-bottom:1px solid #aaa;
+  height:75rem;
 }
 .tree{
-  height:1200px !important;
+  height:75rem !important;
 }
 .four{
   width:100%;
   height:5.875rem;
+  border-bottom: .0625rem solid #eee;
+}
+.four>ul{
+  display: flex;
+  justify-content: space-between;
+}
+.four>ul>li{
+  width:5.859375rem;
+  height:5.875rem;
+}
+.four>ul>li>div>img{
+  width:3.25rem;
+  height:3.25rem;
+}
+.four>ul>li>span{
+  font-size: .75rem;
 }
 </style>
