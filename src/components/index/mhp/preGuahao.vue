01d33ef303dd1dd8680aa438b1eaa13ae61d0dce
Miaohuiping <57703957+Miaohuiping@users.noreply.github.com>
2019-11-14 20:51:32 +0800

    Create preGuahao.vue

---
+++ src/components/index/mhp/preGuahao.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <preTitle></preTitle>
+    <wysearch></wysearch> 
+    <div class="four">
+      <ul>
+        <li class="sanjia">
+          <div>
+            <img src="" alt="">
+          </div>
+          <span>三甲体检</span>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</template>
+<script>
+import preTitle from "./preTitle";
+import wysearch from "./wysearch";
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  components:{
+    "preTitle":preTitle,
+    "wysearch":wysearch,
+  },
+}
+</script>
+<style scoped>
+templete{
+    height:1200px;
+}
+wysearch{
+  border-bottom:1px solid #aaa;
+}
+.tree{
+  height:1200px !important;
+}
+.four{
+  width:100%;
+  height:5.875rem;
+}
+</style>
