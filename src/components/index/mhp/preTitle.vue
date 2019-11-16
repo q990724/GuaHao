65726b5230f8b911bb20a9fb2d99c11006efc41c
mrzhang <1070675659@qq.ocom>
2019-11-16 15:53:12 +0800

    app的样式重置更改之后，修改预约挂号头部标题的样式

--- src/components/index/mhp/preTitle.vue
+++ src/components/index/mhp/preTitle.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 <style scoped>
 .container{
+  padding:0 .610687rem;
   height: 2.6125rem;
   position: fixed;
   top:0rem;
@@ -39,9 +40,12 @@ export default {
   border-bottom:.0625rem solid #eee;
 }
 .goback{
-  position: relative;
-  left:-7.5rem;
+  /* position: relative;
+  left:-7.5rem; */
   font-size: 1.125rem;
   line-height: 2.6125rem;
+}
+.pre_title{
+  padding: 0 9.060305rem;
 }
 </style>
