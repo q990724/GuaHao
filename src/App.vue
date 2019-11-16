ee106d84ae6ab195b8939acf6ff0713a3e4b6f9d
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-16 17:19:53 +0800

    App.vue改样式

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,7 @@ html{
 body{
   background: #f8f8f8;
 }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  box-sizing: border-box;
-  width:100%;
-  height:100%;
-}
+
 *{
   margin:0;
   padding:0;
