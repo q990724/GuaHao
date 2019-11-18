aa311a50654c6922630e2d823cdd6a6de6a171c7
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-18 14:34:30 +0800

    Create top_search.vue

---
+++ src/components/jkh/wtq/top_search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="top">
+    <!-- 组件库 搜索框 -->
+    <van-search placeholder="搜索医生、疾病科普等内容" v-model="value" />
+    <img src="">
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+</style>
