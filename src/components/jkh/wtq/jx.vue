c39f26822079473ae554e94d4ce6164dedb21842
Jade8998 <57703858+Jade8998@users.noreply.github.com>
2019-11-18 21:51:12 +0800

    组件

---
+++ src/components/jkh/wtq/jx.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="jx">
+    <div class="first-row">
+      <div class="left">
+        <div class="img">
+          <img src="../../../../public/images/jkh/top-entry-help.png">
+        </div>
+        <div class="info">
+          <span>乔贵宾 | 主任医师</span>
+          <span>刚刚</span>
+        </div>
+      </div>
+      <div class="right">
+        <span>向TA提问</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+/*第一排*/
+.first-row{
+  display:flex;
+  justify-content: space-between;
+}
+</style>
