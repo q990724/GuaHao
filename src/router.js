34dbb8c42abbec2064eb2f8aeb19cc67fc77d7f1
w1322174203 <57716097+w1322174203@users.noreply.github.com>
2019-11-21 02:05:57 +0800

    tabbar面板修改

--- src/router.js
+++ src/router.js
@@ -12,6 +12,7 @@ import index from "./views/index/index.vue";
 import reg from "./components/me/wf/reg.vue";
 import order2 from "./components/index/wtq/professor/Order2";
 import order_main from "./components/index/wf-zwb/order_main.vue";
+import frame from "./components/index/frame.vue"
 
 Vue.use(Router)
 export default new Router({
@@ -27,7 +28,7 @@ export default new Router({
     { path: "/index",component: index},
     { path: "/reg",component: reg},
     { path: "/order2",component: order2},
-    { path: "/order_main",component:order_main}
-
+    { path: "/order_main",component:order_main},
+    { path: "/frame",component: frame}
   ]
 })
