6128da0124851b69df46641610fa5a9e9a7aec64
q990724 <57699194+q990724@users.noreply.github.com>
2019-11-21 11:39:56 +0800

    所有接口优化
    所有接口，共用一个连接实例

--- SERVER/zy/routes/healthyNumber.js
+++ SERVER/zy/routes/healthyNumber.js
@@ -1,13 +1,12 @@
 const express = require("express");
 var router = express.Router();
-var config = require("../config");
 
 //健康号页面的分页查询数据
 router.get("/healthyNumber", function (req, res) {
     //获取数据 pno pageSize
     var pno = req.query.pno;
     var ps = req.query.pageSize;
-        
+
     //设置数据默认值
     if (!pno || !Number(pno)) { pno = 1 }
     if (!ps) { ps = 5 }
@@ -15,34 +14,21 @@ router.get("/healthyNumber", function (req, res) {
     var offset = (pno - 1) * ps;
     //给pageSize造型整型
     ps = parseInt(ps);
-    config.mongoClient.connect(config.url,{ useNewUrlParser: true, useUnifiedTopology: true },function(err,db){
-        if(err) throw err;
-        var dbo = db.db("app_zhuanyi");
-        /*
-            var query = dbo.collection("healthy_number").find({});
-            query.skip(offset);
-            query.limit(ps);
-            query.exec(function(err,result){
-                res.send({data : result,count : rs.length});
-            })
-        */
-        dbo.collection("healthy_number").find().skip(offset).limit(ps).toArray(function(err,result){
-            if(err){
-                throw err;
-            }else{
-                var totalCount = -1;
-                dbo.collection("healthy_number").find().toArray(function(e,rs){
-                    if(e){
-                        throw e;
-                    }else{
-                        totalCount = rs.length;
-                        res.send({code : 1 , msg : "查询成功" , totalCount , data : result});
-                    }
-                });
-            }
-            
-        });
-        
+    req.db.collection("healthy_number").find().skip(offset).limit(ps).toArray(function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            var totalCount = -1;
+            req.db.collection("healthy_number").find().toArray(function (e, rs) {
+                if (e) {
+                    throw e;
+                } else {
+                    totalCount = rs.length;
+                    res.send({ code: 1, msg: "查询成功", totalCount, data: result });
+                }
+            });
+        }
+
     });
 });
 
